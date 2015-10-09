0b31d5983672c62b64d35789a479f4efe21dea23
readpeta <murakumo.room@gmail.com>
2015-10-09 20:25:31 +0900

    ファイルです

---
+++ main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// アプリケーションをコントロールするモジュール
+var app = require('app');
+// ウィンドウを作成するモジュール
+var BrowserWindow = require('browser-window');
+
+// クラッシュレポート
+require('crash-reporter').start();
+
+// メインウィンドウはGCされないようにグローバル宣言
+var mainWindow = null;
+
+// 全てのウィンドウが閉じたら終了
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// Electronの初期化完了後に実行
+app.on('ready', function() {
+  // メイン画面の表示。ウィンドウの幅、高さを指定できる
+  mainWindow = new BrowserWindow({ title:"todo", width: 800, height: 600});
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
+
+// winuserのタスクでいいことできるかも
+// app.setUserTasks([
+//   {
+//     program: process.execPath,
+//     arguments: '--new-window',
+//     iconPath: process.execPath,
+//     iconIndex: 0,
+//     title: 'New Window',
+//     description: 'Create a new winodw',
+//   }
+// ]);
