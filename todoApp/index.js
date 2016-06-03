46382a8d7bc912404a29f3081c9c3a7cd28c94a9
readpeta <murakumo.room@gmail.com>
2016-06-03 23:22:50 +0900

    live2d add

---
+++ todoApp/index.js
@@ -0,0 +1,41 @@
+/*
+ * index.js
+ */
+
+'use strict';
+
+// アプリケーションをコントロールするモジュール
+var app = require('app');
+// ウィンドウを作成するモジュール
+var BrowserWindow = require('browser-window');
+// 起動 URL
+var currentURL = 'file://' + __dirname + '/index.html';
+
+// クラッシュレポート
+require('crash-reporter').start();
+
+// メインウィンドウ
+var mainWindow = null;
+
+// 全てのウィンドウが閉じたら終了
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// Electronの初期化完了後に実行
+app.on('ready', function() {
+  // メイン画面のサイズ
+  mainWindow = new BrowserWindow({width: 1200, height: 700});
+  // 起動 url を指定
+  mainWindow.loadUrl(currentURL);
+
+  // デベロッパーツールを表示
+  //mainWindow.toggleDevTools();
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
